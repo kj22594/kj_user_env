13f87fc434698f95895f0559641b78a351239eec
kj22594 <kj@simplisafe.com>
2019-04-29 18:41:50 -0400

    Add comment

--- mac_setup.sh
+++ mac_setup.sh
@@ -5,6 +5,6 @@ xargs brew install < brew_requirements.txt
 brew tap caskroom/cask
 xargs brew cask install < cask_requirements.txt
 
-
+# https://github.com/riobard/bash-powerline
 curl https://raw.githubusercontent.com/riobard/bash-powerline/master/bash-powerline.sh > ~/.bash-powerline.sh
 
